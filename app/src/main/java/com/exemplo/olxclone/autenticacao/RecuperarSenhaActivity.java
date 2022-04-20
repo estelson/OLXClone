11bf6ac00e6da3f5f4a15e1c42f90331d97efbd0
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-20 18:10:28 -0300

    157. Programando Tela de Esqueceu a Senha
    Validação de campos e envio de e-mail de reset de senha utilizando o Firebase.

--- app/src/main/java/com/exemplo/olxclone/autenticacao/RecuperarSenhaActivity.java
+++ app/src/main/java/com/exemplo/olxclone/autenticacao/RecuperarSenhaActivity.java
@@ -3,14 +3,50 @@ package com.exemplo.olxclone.autenticacao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.exemplo.olxclone.R;
+import com.exemplo.olxclone.helper.FirebaseHelper;
 
 public class RecuperarSenhaActivity extends AppCompatActivity {
+
+    private EditText edt_email;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar_senha);
+
+        iniciaComponentes();
+    }
+
+    public void validaDados(View view) {
+        String email = edt_email.getText().toString().trim();
+
+        if(!email.isEmpty()) {
+            enviarEmail(email);
+        } else {
+            edt_email.requestFocus();
+            edt_email.setError("Informe o e-Mail");
+        }
+    }
+
+    private void enviarEmail(String email) {
+        FirebaseHelper.getAuth().sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                Toast.makeText(this, "E-mail enviado com sucesso. Verifique sua caixa de entrada.", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Erro ao enviar o e-Mail. Motivo: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void iniciaComponentes() {
+        edt_email = findViewById(R.id.edt_email);
+        progressBar = findViewById(R.id.progressBar);
     }
 }
