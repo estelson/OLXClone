aa53cc4a749b193812334965cbce0e208b95387d
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-20 20:13:10 -0300

    162. Criando Fragments (Home - Meus Anúncios - Favoritos - Minha Conta)

---
+++ app/src/main/java/com/exemplo/olxclone/fragments/MinhaContaFragment.java
@@ -0,0 +1,22 @@
+package com.exemplo.olxclone.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.exemplo.olxclone.R;
+
+public class MinhaContaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_minha_conta, container, false);
+    }
+
+}
