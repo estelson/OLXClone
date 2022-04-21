16b4c824fe2cd2346b7ebd11a7a684081e575d6c
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-21 11:13:47 -0300

    171. Criando Eventos de Cliques Fragment Minha Conta

--- app/src/main/java/com/exemplo/olxclone/fragments/MinhaContaFragment.java
+++ app/src/main/java/com/exemplo/olxclone/fragments/MinhaContaFragment.java
@@ -1,5 +1,6 @@
 package com.exemplo.olxclone.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,14 +10,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.exemplo.olxclone.R;
+import com.exemplo.olxclone.activities.EnderecoActivity;
+import com.exemplo.olxclone.activities.PerfilActivity;
 
 public class MinhaContaFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_minha_conta, container, false);
+        View view = inflater.inflate(R.layout.fragment_minha_conta, container, false);
+
+        configCliques(view);
+
+        return view;
+    }
+
+    private void configCliques(View view) {
+        view.findViewById(R.id.menu_perfil).setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), PerfilActivity.class));
+        });
+
+        view.findViewById(R.id.menu_endereco).setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), EnderecoActivity.class));
+        });
     }
 
 }
