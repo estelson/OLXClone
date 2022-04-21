601a90de35a8085227814a53b7ec0055018bb309
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-21 17:06:54 -0300

    183. Configurando Opção de Deslogar do App

--- app/src/main/java/com/exemplo/olxclone/fragments/MinhaContaFragment.java
+++ app/src/main/java/com/exemplo/olxclone/fragments/MinhaContaFragment.java
@@ -8,17 +8,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.exemplo.olxclone.R;
 import com.exemplo.olxclone.activities.EnderecoActivity;
+import com.exemplo.olxclone.activities.MainActivity;
 import com.exemplo.olxclone.activities.PerfilActivity;
+import com.exemplo.olxclone.autenticacao.LoginActivity;
+import com.exemplo.olxclone.helper.FirebaseHelper;
 
 public class MinhaContaFragment extends Fragment {
+
+    private TextView text_conta;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_minha_conta, container, false);
+
+        iniciaComponentes(view);
 
         configCliques(view);
 
@@ -33,6 +41,26 @@ public class MinhaContaFragment extends Fragment {
         view.findViewById(R.id.menu_endereco).setOnClickListener(v -> {
             startActivity(new Intent(getActivity(), EnderecoActivity.class));
         });
+
+        if(FirebaseHelper.getAutenticado()) {
+            text_conta.setText("Sair");
+        } else {
+            text_conta.setText("Clique aqui");
+        }
+
+        text_conta.setOnClickListener(v -> {
+            if(FirebaseHelper.getAutenticado()) {
+                FirebaseHelper.getAuth().signOut();
+
+                startActivity(new Intent(getActivity(), MainActivity.class));
+            } else {
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
+    }
+
+    private void iniciaComponentes(View view) {
+        text_conta = view.findViewById(R.id.text_conta);
     }
 
 }
