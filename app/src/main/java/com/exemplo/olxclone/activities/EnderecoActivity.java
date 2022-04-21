d4186ab74705a9cf22ff13cecf905493ec42d315
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-21 11:01:15 -0300

    170. Criando Layout Tela de Endereço do Usuário

---
+++ app/src/main/java/com/exemplo/olxclone/activities/EnderecoActivity.java
@@ -0,0 +1,16 @@
+package com.exemplo.olxclone.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.exemplo.olxclone.R;
+
+public class EnderecoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_endereco);
+    }
+}
