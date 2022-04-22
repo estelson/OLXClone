eece964c3aede64184696fa0138ab8e8435d1bf1
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-22 15:43:14 -0300

    186. Criando Layout Tela Formulário do Anúncio Parte 1

--- app/src/main/java/com/exemplo/olxclone/activities/FormAnuncioActivity.java
+++ app/src/main/java/com/exemplo/olxclone/activities/FormAnuncioActivity.java
@@ -3,6 +3,8 @@ package com.exemplo.olxclone.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.exemplo.olxclone.R;
 
@@ -12,5 +14,13 @@ public class FormAnuncioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_anuncio);
+
+        iniciaComponentes();
     }
+
+    private void iniciaComponentes() {
+        TextView text_toolbar = findViewById(R.id.text_toolbar);
+        text_toolbar.setText("Novo anúncio");
+    }
+
 }
