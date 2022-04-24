9608fcdd83fd92655095b3287904045a99da86f3
Estelson Medeiros Pereira <estelson@gmail.com>
2022-04-24 12:46:51 -0300

    209. Listando Meus Anúncios Parte 1

---
+++ app/src/main/java/com/exemplo/olxclone/adapter/AnuncioAdapter.java
@@ -0,0 +1,74 @@
+package com.exemplo.olxclone.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.exemplo.olxclone.R;
+import com.exemplo.olxclone.model.Anuncio;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class AnuncioAdapter extends RecyclerView.Adapter<AnuncioAdapter.MyViewHolder> {
+
+    private List<Anuncio> anuncioList;
+    private OnClickListener onClickListener;
+
+    public AnuncioAdapter(List<Anuncio> anuncioList, OnClickListener onClickListener) {
+        this.anuncioList = anuncioList;
+        this.onClickListener = onClickListener;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.anuncio_item, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Anuncio anuncio = anuncioList.get(position);
+
+        Picasso.get().load(anuncio.getUrlImagens().get(0)).into(holder.img_anuncio);
+
+        holder.text_titulo.setText(anuncio.getTitulo());
+        //holder.text_valor.setText(anuncio.getValor());
+        holder.text_local.setText(anuncio.getLocal().getLocalidade());
+    }
+
+    @Override
+    public int getItemCount() {
+        return anuncioList.size();
+    }
+
+    public interface OnClickListener {
+        void OnClick(Anuncio anuncio);
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView img_anuncio;
+
+        TextView text_titulo;
+        TextView text_valor;
+        TextView text_local;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            img_anuncio = itemView.findViewById(R.id.img_anuncio);
+
+            text_titulo = itemView.findViewById(R.id.text_titulo);
+            text_valor = itemView.findViewById(R.id.text_valor);
+            text_local = itemView.findViewById(R.id.text_local);
+        }
+    }
+
+}
